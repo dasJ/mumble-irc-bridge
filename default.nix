eaec0b05d596cb3d4dda03260e10eab1d8ca1038
Jörg Thalheim <joerg@thalheim.io>
2022-05-25 23:50:12 +0200

    drop libopus

--- default.nix
+++ default.nix
@@ -2,9 +2,6 @@ with import <nixpkgs> {};
 pkgs.buildGoModule {
   name = "mumble-irc-bridge";
   src = ./.;
-  buildInputs = [
-    libopus
-  ];
   vendorSha256 = "sha256-AwJbfWoK8vUvzLF9+zS4rBZ6K9sdSBCqcfKpWDNlwPs=";
   proxyVendor = true;
 }
